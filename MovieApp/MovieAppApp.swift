b1f5881d8c2f55f0194a1eb287431520d11ef045
joricky91 <jonathanricky91@gmail.com>
2023-01-19 14:22:39 +0700

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Jonathan Ricky Sandjaja on 19/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
