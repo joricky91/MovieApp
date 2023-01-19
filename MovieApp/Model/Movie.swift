654181d499a56694e510c3b495b9f79efddd113f
joricky91 <jonathanricky91@gmail.com>
2023-01-19 15:05:58 +0700

    feat: create movie struct

---
+++ MovieApp/Model/Movie.swift
@@ -0,0 +1,30 @@
+//
+//  Movie.swift
+//  MovieApp
+//
+//  Created by Jonathan Ricky Sandjaja on 19/01/23.
+//
+
+import Foundation
+
+struct MovieResponse: Codable {
+    var response: [Movie]
+}
+
+struct Movie: Codable {
+    let id: Int
+    let title: String
+    let releaseDate: String
+    let poster: String
+    let language: String
+    let overview: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case title
+        case releaseDate = "release_date"
+        case poster = "poster_path"
+        case language = "original_language"
+        case overview
+    }
+}
