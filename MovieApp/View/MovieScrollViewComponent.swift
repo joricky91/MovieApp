16576200ea9a100797f9eef3de1e3ec28967af55
joricky91 <jonathanricky91@gmail.com>
2023-01-23 20:13:25 +0700

    fix: fixing text appeared black in dark mode

--- MovieApp/View/MovieScrollViewComponent.swift
+++ MovieApp/View/MovieScrollViewComponent.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct MovieScrollViewComponent: View {
     var movie: [Movie] = []
     var title: String = ""
+    @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -22,7 +23,7 @@ struct MovieScrollViewComponent: View {
                     ForEach(movie, id: \.id) { movie in
                         NavigationLink(destination: MovieDetails(movieID: movie.id)) {
                             MoviePoster(imageURL: "https://image.tmdb.org/t/p/w1280/\(movie.poster ?? "")", movieTitle: movie.title)
-                                .foregroundColor(.black)
+                                .foregroundColor(colorScheme == .light ? .black : .white)
                         }
                     }
                 }
